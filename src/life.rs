b89558250633b56da94bedf688c2c0534a3478d7
Jonathan Neufeld <jon_neufeld@pm.me>
2023-08-09 18:05:50 -0700

    Add tests for debugging
    I suspected neighbour counting was broken, but after adding these tests
    I'll need to look elsewhere.

--- src/life.rs
+++ src/life.rs
@@ -4,7 +4,6 @@ use std::{
     time::Duration,
 };
 
-/// The board on which to play the game of life
 pub struct Life {
     width: usize,
     height: usize,
@@ -13,14 +12,13 @@ pub struct Life {
 }
 
 impl Life {
-    /// Create a new game of life
     pub fn new(width: usize, height: usize, seed: Vec<(usize, usize)>) -> Life {
         if width == 0 {
-            panic!("Width must be greater than zero (received {})", width);
+            panic!("Width must be greater than zero (received {width})");
         }
 
         if height == 0 {
-            panic!("Height must be greater than zero (received {})", height);
+            panic!("Height must be greater than zero (received {height})");
         }
 
         let mut cells = Vec::new();
@@ -37,10 +35,10 @@ impl Life {
 
         for (x, y) in seed {
             if x > width - 1 {
-                panic!("Seed cell is outside board. x={} but width={}.", x, width);
+                panic!("Seed cell is outside board. x={x} but width={width}.");
             }
             if y > height - 1 {
-                panic!("Seed cell is outside board. y={} but width={}.", y, height);
+                panic!("Seed cell is outside board. y={y} but height={height}.");
             }
 
             cells[y][x].set(true);
@@ -54,15 +52,15 @@ impl Life {
         }
     }
 
-    /// Play the game of life
+    /// Plays the game by printing the result and pausing between steps.
     pub fn play(&mut self) {
-        println!("{}", self);
+        println!("{self}");
 
         while self.can_continue() {
             self.tick();
             self.steps += 1;
 
-            println!("{}", self);
+            println!("{self}");
 
             let pause_time = Duration::from_secs(2);
 
@@ -70,26 +68,22 @@ impl Life {
         }
     }
 
-    /// Continue if there are any living cells
+    /// Returns true when any cell is alive.
     fn can_continue(&self) -> bool {
         self.cells.iter().flatten().any(|cell| cell.get())
     }
 
-    /// A tick is an iteration of the game. Every iteration, update all cells according to the
-    /// rules of the game.
+    /// A tick is an iteration of the game. Every iteration, update all cells
+    /// according to the rules of the game.
     fn tick(&mut self) {
         for (y, row) in self.cells.iter().enumerate() {
             for (x, cell) in row.iter().enumerate() {
                 let is_alive = cell.get();
                 let neighbours = self.count_neighbours(x, y);
 
-                // Kill living cells with too many or too few neighbours
                 if is_alive && (neighbours > 3 || neighbours < 2) {
                     cell.set(false);
-                }
-
-                // Dead cells with the right neighbours come to life
-                if !is_alive && neighbours == 3 {
+                } else if !is_alive && neighbours == 3 {
                     cell.set(true);
                 }
             }
@@ -98,60 +92,38 @@ impl Life {
 
     /// Counts the number of living cells in the Moore neighbourhood
     fn count_neighbours(&self, cell_x: usize, cell_y: usize) -> u8 {
-        // The implementation is fussy over types and edge cases. The code is deliberately verbose
-        // to help make sense of the details.
+        let mut result = 0;
+
         let max_width = self.width - 1;
         let max_height = self.height - 1;
 
-        // Enumerate the row and column indices of potential neighbours. Some spill over the edge of
-        // the board. Using `checked_` methods on `usize` returns an `Option` where `Some(_)` returns
-        // an actual value while `None` indicates an overflow (i.e., the index is off the board).
+        // Enumerate the row and column indices of potential neighbours. Some
+        // spill over the edge of the board. Using `checked_` methods on `usize`
+        // returns an `Option` where `Some(_)` returns an actual value while
+        // `None` indicates an overflow (i.e., the index is off the board).
         let x_indices = [cell_x.checked_sub(1), cell_x.checked_add(1), Some(cell_x)];
         let y_indices = [cell_y.checked_sub(1), cell_y.checked_add(1), Some(cell_y)];
 
-        let mut result = 0;
-
         for y in y_indices {
-            // Skip indices of the `None` variant since these indicate an index off the board (i.e., it is)
-            // it is too small)
-            if y.is_none() {
-                continue;
-            }
-
-            // Since the variant is not `None` it is safe unwrap the value
-            let y = y.unwrap();
-
-            // Ensure the value is on the board in the other direction (i.e., it isn't too big)
-            if y > max_height {
-                continue;
-            }
-
-            // The logic for each column index is the same as the code above. If all conditions pass, the
-            // tuple of indices indexes a cell on the board.
             for x in x_indices {
-                if x.is_none() {
+                if y.is_none() || x.is_none() {
                     continue;
                 }
 
+                let y = y.unwrap();
                 let x = x.unwrap();
 
-                if x > max_width {
+                if x > max_width || y > max_height {
                     continue;
                 }
 
-                // Lastly, skip if this is `(i, j)` since that is the cell for which we are counting the
-                // neighbours
                 if x == cell_x && y == cell_y {
                     continue;
                 }
 
-                // If the cell is dead then continue
-                if !self.cells[y][x].get() {
-                    continue;
+                if self.cells[y][x].get() {
+                    result += 1;
                 }
-
-                // Otherwise count the neighbour as live
-                result += 1;
             }
         }
 
@@ -165,13 +137,78 @@ impl fmt::Display for Life {
 
         for row in &self.cells {
             for cell in row {
-                let value = if cell.get() { "x" } else { "." };
-                cell_displays.push_str(value);
+                let c = if cell.get() { 'x' } else { '.' };
+                cell_displays.push(c);
             }
 
             cell_displays.push('\n');
         }
 
         writeln!(f, "{}\nStep: {}", cell_displays, self.steps)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty_seed() {
+        let seed = vec![];
+        let life = Life::new(10, 10, seed);
+
+        assert_eq!(0, life.count_neighbours(0, 0));
+    }
+
+    #[test]
+    fn glider_one() {
+        let seed = vec![(1, 1), (1, 3), (2, 2), (2, 3), (3, 2)];
+        let life = Life::new(10, 10, seed);
+
+        assert_eq!(1, life.count_neighbours(0, 0));
+        assert_eq!(1, life.count_neighbours(1, 0));
+        assert_eq!(1, life.count_neighbours(2, 0));
+        assert_eq!(0, life.count_neighbours(3, 0));
+
+        assert_eq!(1, life.count_neighbours(0, 1));
+        assert_eq!(1, life.count_neighbours(1, 1));
+        assert_eq!(3, life.count_neighbours(2, 1));
+        assert_eq!(2, life.count_neighbours(3, 1));
+
+        assert_eq!(2, life.count_neighbours(0, 2));
+        assert_eq!(4, life.count_neighbours(1, 2));
+        assert_eq!(4, life.count_neighbours(2, 2));
+        assert_eq!(2, life.count_neighbours(3, 2));
+
+        assert_eq!(1, life.count_neighbours(0, 3));
+        assert_eq!(2, life.count_neighbours(1, 3));
+        assert_eq!(3, life.count_neighbours(2, 3));
+        assert_eq!(3, life.count_neighbours(3, 3));
+    }
+
+    #[test]
+    fn glider_two() {
+        let seed = vec![(1, 2), (2, 3), (3, 1), (3, 2), (3, 3)];
+        let life = Life::new(10, 10, seed);
+
+        assert_eq!(0, life.count_neighbours(0, 0));
+        assert_eq!(0, life.count_neighbours(1, 0));
+        assert_eq!(1, life.count_neighbours(2, 0));
+        assert_eq!(1, life.count_neighbours(3, 0));
+
+        assert_eq!(1, life.count_neighbours(0, 1));
+        assert_eq!(1, life.count_neighbours(1, 1));
+        assert_eq!(3, life.count_neighbours(2, 1));
+        assert_eq!(1, life.count_neighbours(3, 1));
+
+        assert_eq!(1, life.count_neighbours(0, 2));
+        assert_eq!(1, life.count_neighbours(1, 2));
+        assert_eq!(5, life.count_neighbours(2, 2));
+        assert_eq!(3, life.count_neighbours(3, 2));
+
+        assert_eq!(1, life.count_neighbours(0, 3));
+        assert_eq!(2, life.count_neighbours(1, 3));
+        assert_eq!(3, life.count_neighbours(2, 3));
+        assert_eq!(2, life.count_neighbours(3, 3));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,30 @@ mod life;
 use crate::life::Life;
 
 fn main() {
-    let seed = glider(50, 10);
-
-    let mut life = Life::new(100, 20, seed);
+    let seed = glider_one(1, 1);
+    let mut life = Life::new(50, 20, seed);
 
     life.play();
 
     println!("Life ended after {} generations", life.steps);
 }
 
-fn glider(x: usize, y: usize) -> Vec<(usize, usize)> {
+fn glider_one(x: usize, y: usize) -> Vec<(usize, usize)> {
     vec![
+        (x, y),
+        (x, y + 2),
+        (x + 1, y + 1),
         (x + 1, y + 2),
-        (x + 2, y + 3),
-        (x + 3, y + 1),
-        (x + 3, y + 2),
-        (x + 3, y + 3),
+        (x + 2, y + 1),
+    ]
+}
+
+fn glider_two(x: usize, y: usize) -> Vec<(usize, usize)> {
+    vec![
+        (x, y + 1),
+        (x + 1, y + 2),
+        (x + 2, y),
+        (x + 2, y + 1),
+        (x + 2, y + 2),
     ]
 }
